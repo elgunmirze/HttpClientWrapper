4899effcb77c24281b65bdb44ee4d661b5bda380
elgun.mirza <elgun.mirza@avanade.com>
2018-08-22 13:56:02 +0300

    Added exception

---
+++ HttpClientWrapper/Exceptions/HttpClientExceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+
+namespace HttpClientWrapper.Exceptions
+{
+    public static class HttpClientExceptions
+    {
+        public static Exception ThrowException(HttpResponseMessage response)
+        {
+            var exception = new Exception($"Server returned an error. StatusCode : {response.StatusCode}");
+
+            exception.Data.Add("StatusCode", response.StatusCode);
+
+            throw exception;
+        }
+    }
+}
